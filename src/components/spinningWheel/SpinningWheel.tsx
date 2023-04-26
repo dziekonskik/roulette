4e7e08bc8832d064f493dcc5e9cdf2aa91504c67
Konrad Dziekonski <jaseveen@gmail.com>
2023-04-26 07:40:08 +0200

    feat(babylon): update babylon and add havok physics engine

--- src/components/spinningWheel/SpinningWheel.tsx
+++ src/components/spinningWheel/SpinningWheel.tsx
@@ -1,22 +1,36 @@
 import {
   ArcRotateCamera,
+  DirectionalLight,
   HemisphericLight,
   Mesh,
   Scene,
   Vector3,
 } from "@babylonjs/core";
+import { HavokPlugin } from "@babylonjs/core/Physics";
+import { observer } from "mobx-react-lite";
+import type { WheelState } from "../../store/gameStore/types";
+import { useStore } from "../../store/rootStoreProvider";
 import { SceneComponent } from "./SceneComponent";
 import styles from "./spinningWheel.module.scss";
+import { createBall } from "./utils/createBall";
 import { createGround } from "./utils/createGround";
 import { createSkyBox } from "./utils/createSkyBox";
 import { createSpinningWheel } from "./utils/createSpinningWheel";
 
-let spinnigWall: Mesh;
+import HavokPhysics from "@babylonjs/havok";
+
+async function getInitializedHavok() {
+  return await HavokPhysics();
+}
+
 let spinningBase: Mesh;
+let ball: Mesh;
 
 const onSceneReady = (scene: Scene) => {
-  scene.enablePhysics();
-
+  scene.debugLayer.show();
+  new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+  const light = new DirectionalLight("light", new Vector3(-1, -2, -2), scene);
+  light.intensity = 0.3;
   const camera = new ArcRotateCamera(
     "camera",
     -Math.PI * 0.5,
@@ -28,33 +42,48 @@ const onSceneReady = (scene: Scene) => {
   camera.setTarget(Vector3.Zero());
   const canvas = scene.getEngine().getRenderingCanvas();
   camera.attachControl(canvas, true);
-  new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-  // light.intensity = 0.7;
   createSkyBox(scene);
   createGround(scene);
+  ball = createBall(scene);
+  ball.position.y = 0.7;
+  ball.position.x = 1;
   const spinningWheel = createSpinningWheel(scene);
-  spinnigWall = spinningWheel.spinnigWall;
   spinningBase = spinningWheel.spinningBase;
+
+  const havokInstance = getInitializedHavok();
+  scene.enablePhysics(
+    new Vector3(0, -9.81, 0),
+    new HavokPlugin(true, havokInstance)
+  );
 };
 
-const onRender = (scene: Scene) => {
+const onRender = (scene: Scene, wheelState: WheelState) => {
   if (spinningBase !== undefined) {
     const deltaTimeInMillis = scene.getEngine().getDeltaTime();
 
-    const rpm = 2;
-    spinningBase.rotation.y +=
-      (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+    if (wheelState === "idle") {
+      const rpm = 2;
+      spinningBase.rotation.y +=
+        (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
+    }
+    if (wheelState === "spinning") {
+    }
   }
 };
 
-export const SpinningWheel = () => (
-  <div className={styles.spinningWheelcontainer}>
-    <SceneComponent
-      antialias
-      onSceneReady={onSceneReady}
-      onRender={onRender}
-      id="my-canvas"
-      className={styles.canvas}
-    />
-  </div>
-);
+export const SpinningWheel = observer(() => {
+  const {
+    gameStore: { wheelState },
+  } = useStore();
+  return (
+    <div className={styles.spinningWheelcontainer}>
+      <SceneComponent
+        antialias
+        onSceneReady={onSceneReady}
+        onRender={(scene) => onRender(scene, wheelState)}
+        id="my-canvas"
+        className={styles.canvas}
+      />
+    </div>
+  );
+});
